fd2be8b50a8a48db616553cce8a257ec222d841d
frapolella <francescopolella2@gmail.com>
2026-04-29 11:47:05 +0200

    Update hefrag.py

--- BigDFTtutorials/hefrag.py
+++ BigDFTtutorials/hefrag.py
@@ -1,14 +1,14 @@
-from BigDFT.Atoms import Atom
-at1 = Atom({'r': [1.0, 0.0, 0.0], 'sym':"He", 'units':'bohr', "nzion": 2})
+from BigDFT.Atoms import Atom   #---> callss Atom to generate a small He cluster
+at1 = Atom({'r': [1.0, 0.0, 0.0], 'sym':"He", 'units':'bohr', "nzion": 2}) #---> at1 = Atom({'r': [x, y, z], atsymbol, measure units, electron number})
 at2 = Atom({'r': [3.0, 0.0, 0.0], 'sym':"He", 'units':'bohr', "nzion": 2})
 
-from BigDFT.Fragments import Fragment
-frag = Fragment()
-frag.append(at1)
+from BigDFT.Fragments import Fragment #---> call Fragment
+frag = Fragment() #---> generates a fragment ---> all atom in a fragment may display same color
+frag.append(at1) #---> appeend at1 and at2 to the fragment
 frag.append(at2)
 
 
-for at in frag:
+for at in frag: #---> prints at in fragment
   print(dict(at))
 
   print(frag.centroid)
